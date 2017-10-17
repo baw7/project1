6d7527ffba6f38cfc5db075bbb9e29475f362699
baw7 <baw7@njit.edu>
2017-10-16 21:18:21 -0400

    all

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form
+action="https://web.njit.edu/~baw7/project1/uploads/uploads.php" method="post"
+enctype="multipart/form-data">
+Select file to upload:
+<input type="file" name="fileToUpload" id="fileToUpload">
+<input type="submit" value="Upload File" name="submit">
+</form>
+
+
+</body>
+</html>
+
