e6c6616dc0b00199006dad6e5670675e9fb15d26
baw7 <baw7@njit.edu>
2017-10-16 22:53:08 -0400

    ugh

--- index.php
+++ index.php
@@ -1,9 +1,42 @@
+<?php
+$allowedExts = array("csv", "doc");
+$allowedTypes = array("text/csv",'..','..',"image/doc");
+
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((in_array($_FILES["file"]["type"], $allowedTypes))
+   && ($_FILES["file"]["size"] < 20000)
+      && in_array($extension, $allowedExts))
+      {
+      if ($_FILES["file"]["error"] > 0)
+      {
+         echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+	 }
+	 else
+	 {
+
+	 if (file_exists("upload/" . $_FILES["file"]["name"]))
+	   {
+	     echo $_FILES["file"]["name"] . " already exists. ";
+	       }
+	         else
+		   {
+		       move_uploaded_file($_FILES["file"]["tmp_name"],
+		       "upload/" . $_FILES["file"]["name"]);
+		           header('Location: forwardpage.html');
+			     }
+			       }
+			       }
+			       else
+			       {
+			       echo "Invalid file" };
+			       ?>
 <!DOCTYPE html>
 <html>
 <body>
 
 <form
-action="https://web.njit.edu/~baw7/project1/uploads/uploads.php" method="post"
+action="https://web.njit.edu/~baw7/project1/uploads/forwardpage.html" method="post"
 enctype="multipart/form-data">
 Select file to upload:
 <input type="file" name="fileToUpload" id="fileToUpload">
