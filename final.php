af1d2ed58f072a4cbae549ff873c9bb6eb78c822
baw7 <baw7@njit.edu>
2017-10-22 10:08:43 -0400

    hi

--- final.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$file = 'http://web.njit.edu/~baw7/project1/index.php?page=table&filename=yourfilename';
-$data = csv_to_array($file,array('header_row'=>true));
-echo '<pre>'.print_r($data,true).'</pre>';
-
-?>
-
